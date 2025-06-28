76667928755d75ec09d5fd8ef3e664a74baa73d2
ishwar <96837761+ishwarx7@users.noreply.github.com>
2025-06-28 12:48:44 +0000

    nav done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
-function showMessage() {
-  alert("Hello from JavaScript!");
-}
+// header sidebar
+document.addEventListener("DOMContentLoaded", () => {
+      const toggle = document.querySelector(".menu-toggle");
+      const navLinks = document.querySelector(".nav-links");
+
+      toggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+      });
+
+      document.addEventListener("click", (e) => {
+        if (e.target.classList.contains("close-btn")) {
+          navLinks.classList.remove("active");
+        }
+      });
+    });
